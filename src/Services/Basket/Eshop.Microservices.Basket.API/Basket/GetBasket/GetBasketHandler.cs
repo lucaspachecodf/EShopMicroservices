05541440df02f5f09576a41db0da289257a9a855
Lucas dos Santos Pacheco <lucaspachecodf@gmail.com>
2024-06-12 21:11:08 -0300

    Implementation API Basket

---
+++ src/Services/Basket/Eshop.Microservices.Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Eshop.Microservices.Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+
+    public class GetBasketHandler
+    {
+        public class GetBasketQueryHandler() : IQueryHandler<GetBasketQuery, GetBasketResult>
+        {
+            public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+            {
+                return new GetBasketResult(new ShoppingCart("Teste"));
+            }
+        }
+    }
+}
---
+++ src/Services/Basket/Eshop.Microservices.Basket.API/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using Eshop.Microservices.Basket.API.Models;
+global using Eshop.Microservices.BuildingBlocks.CQRS;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using FluentValidation;
+global using Eshop.Microservices.BuildingBlocks.Behaviors;
