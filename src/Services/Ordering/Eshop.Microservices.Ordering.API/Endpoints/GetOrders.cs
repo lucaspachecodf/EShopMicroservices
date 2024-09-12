a16fdaa6e072a5d93ba931a3712eb6bc3b29e82d
Lucas dos Santos Pacheco <lucaspachecodf@gmail.com>
2024-09-11 21:11:49 -0300

    Create endpoint GetOrders

---
+++ src/Services/Ordering/Eshop.Microservices.Ordering.API/Endpoints/GetOrders.cs
@@ -0,0 +1,32 @@
+﻿using Carter;
+using Eshop.Microservices.BuildingBlocks.Pagination;
+using Eshop.Microservices.Ordering.Application.Dtos;
+using Eshop.Microservices.Ordering.Application.Orders.Queries.GetOrders;
+using Mapster;
+using MediatR;
+
+namespace Eshop.Microservices.Ordering.API.Endpoints
+{
+    public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+    public class GetOrders : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersQuery(request));
+
+                var response = result.Adapt<GetOrdersResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetOrders")
+            .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Orders")
+            .WithDescription("Get Orders");
+        }
+    }
+}
