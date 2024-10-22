b3c9787e9f3d8e7f2d10574dc0aeb85feae591ec
Lucas dos Santos Pacheco <lucaspachecodf@gmail.com>
2024-10-21 22:31:33 -0300

    Create web client aplication;
    Install and configuration Refit HttpClientFactory;

---
+++ src/WebApps/Eshop.Shopping.Web/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Eshop.Shopping.Web.Models.Catalog;
+global using Refit;
+global using Eshop.Shopping.Web.Services;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
